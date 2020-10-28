d24fc1bfe5176f0dc4436c1b15214041f8e504fc
Victor Hugo Oliveira <victorhugomartins28@gmail.com>
2020-10-28 17:16:15 -0300

    Create main.cpp

---
+++ structHerdandoMapEClass/main.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <map>
+#include <iterator>
+
+class Teste
+{
+
+public:
+    Teste() : value(10), nome("Azul")
+    {
+
+    }
+
+    ~Teste();
+
+    int value;
+    std::string nome;
+
+};
+
+Teste::
+~Teste()
+{
+    std::cout << "Objeto destruido" << std::endl;
+}
+
+struct DOIDERA : std::map<std::string, int>, Teste
+{
+    float mexerica;
+
+    void setValue(float val)
+    {
+        mexerica = val;
+    }
+
+    float getValue()
+    {
+        return mexerica;
+    }
+
+};
+
+DOIDERA::iterator itD;
+
+int main()
+{
+
+    //Declarando a struct DOIDERA
+    DOIDERA d;
+
+    //Chamando valores definidas e instanciadas na classe, Teste herdada na struct DOIDERA
+    std::cout << d.value << " " << d.nome << std::endl << std::endl;
+
+    //Dando valores de map para o objeto
+    d.insert(std::pair<std::string, int>("Abacate", 1));
+    d.insert(std::pair<std::string, int>("Moises", 2));
+    d.insert(std::pair<std::string, int>("Ronaldo", 3));
+    d.insert(std::pair<std::string, int>("Abobra", 3));
+    d.insert(std::pair<std::string, int>("Dinamarca", 5));
+
+    //printando valores criados
+    std::cout << "Printando valores\n";
+    for(itD = d.begin(); itD != d.end(); itD++)
+    {
+        std::cout << itD->first << " | " << itD->second << std::endl;
+    }
+
+    //setando valor com o método da struct DOIDERA
+    d.setValue(7);
+
+    //printando valor com o método da struct DOIDERA
+    std::cout << d.getValue() << std::endl;
+
+    //procurando o iterator de key 'Ronaldo'
+    itD = d.find("Ronaldo");
+
+    //printando valores de find
+    std::cout << "Aplicando find: " << itD->first << " | " << itD->second << std::endl << std::endl;
+
+    //excluindo valor por iterator
+    d.erase(itD);
+
+    return 0;
+}
