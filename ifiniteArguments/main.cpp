d0d07b341fffed379b20ac05fd4204cd978efe29
Victor Hugo Oliveira <victorhugomartins28@gmail.com>
2022-05-15 06:44:41 -0300

    updates

--- ifiniteArguments/main.cpp
+++ ifiniteArguments/main.cpp
@@ -4,7 +4,7 @@
 //Last call with no arguments in the recursive call
 void printar()
 {
-	std::cout << "\n endCalled" << std::endl;
+	std::cout << "\nendCalled" << std::endl;
 }
 
 //Head tail concept
@@ -12,17 +12,19 @@ template<typename Head, typename ... Tail>
 void printar(Head param, const Tail&... rest)
 {
 	
-	std::cout << param << " " << std::endl;
+	std::cout << param << " ";
 	//char word[] = "Uma mistura de";
 	//std::cout << param << " : " << sizeof(word) << std::endl;
 	
 	//it will call recursivelly
-	output( rest... );	
+	printar( rest... );	
 }
+
 
 int main()
 {	
 	printar( "Uma mistura de", 3, "bonecos alegres e", 2 ,"gemas ascendentes é mais do que", 9, "suficientes.");
+	
 	
 	return 0;	
 }
